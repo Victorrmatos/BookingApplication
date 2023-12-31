a2c67cff533fd291212d9e541c9fc3a581c27f46
victorreism <victorreism@gmail.com>
2023-12-31 23:51:52 +0000

    final

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,137 +1,137 @@
 <template>
   <nav
-    class="navbar transparent-70 mb-5"
-    aria-label="main navigation"
-    role="navigation"
-    :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)' }"
-    
+  class="navbar transparent-70 mb-5"
+  aria-label="main navigation"
+  role="navigation"
+  :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)' }"
+  
   >
   <div class="container is-max-desktop px-2">
-      <div class="navbar-brand">
-        <div class="navbar-item is-size-4 is-family-monospace"
-     :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)'}">
-  {{ activePageName }}
+    <div class="navbar-brand">
+      <div class="navbar-item is-size-4 is-family-monospace"
+      :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)'}">
+      {{ activePageName }}
+    </div>
+    
+    <a
+    @click.prevent="showMobileNav = !showMobileNav"
+    class="navbar-burger"
+    :class="{ 'is-active' : showMobileNav }"
+    aria-expanded="false"
+    aria-label="menu"
+    data-target="navbarBasicExample"
+    role="button"
+    :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)'}">
+    
+    
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+  </a>
 </div>
 
-        <a
-          @click.prevent="showMobileNav = !showMobileNav"
-          class="navbar-burger"
-          :class="{ 'is-active' : showMobileNav }"
-          aria-expanded="false"
-          aria-label="menu"
-          data-target="navbarBasicExample"
-          role="button"
-          :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)'}">
+<div
+id="navbarBasicExample"
+class="navbar-menu"
+:class="{ 'is-active' : showMobileNav }"
 
-        
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div
-        id="navbarBasicExample"
-        class="navbar-menu"
-        :class="{ 'is-active' : showMobileNav }"
-        
-      >
+>
 
 
-        <div class="navbar-end" >
-          <RouterLink
-            to="/"
-            class="navbar-item"
-            active-class="is-active"
-            @click.prevent="toggleMobileNav" 
-            :style="{ backgroundColor: $route.path === '/services' ? 'var(--text-color)' : 'transparent', 
-        color: $route.path === '/services' ? 'var(--background-color)' : 'var(--text-color)'}"
-     
-          >
-            New Booking
-          </RouterLink>
-        
-          <RouterLink
-            to="/adminServices"
-            class="navbar-item"
-            active-class="is-active"
-            @click.prevent="toggleMobileNav"
-            :style="{ backgroundColor: $route.path === '/adminServices' ? 'var(--text-color)' : 'transparent', 
-        color: $route.path === '/adminServices' ? 'var(--background-color)' : 'var(--text-color)'}"
-          >
-            Admin Services
-          </RouterLink>
-          <RouterLink
-            to="/adminDates"
-            class="navbar-item"
-            active-class="is-active"
-            @click.prevent="toggleMobileNav"
-            :style="{ backgroundColor: $route.path === '/adminDates' ? 'var(--text-color)' : 'transparent', 
-        color: $route.path === '/adminDates' ? 'var(--background-color)' : 'var(--text-color)'}"
-          >
-            Admin Dates
-          </RouterLink>
-          <RouterLink
-            to="/clients"
-            class="navbar-item"
-            active-class="is-active"
-            @click.prevent="toggleMobileNav"
-            style="color: var(--text-color)"
-            :style="{ backgroundColor: $route.path === '/clients' ? 'var(--text-color)' : 'transparent', 
-        color: $route.path === '/clients' ? 'var(--background-color)' : 'var(--text-color)'}"
-          >
-            Clients
-          </RouterLink>
-          <RouterLink
-            to="/bookings"
-            class="navbar-item"
-            active-class="is-active"
-            @click.prevent="toggleMobileNav"
-            :style="{ backgroundColor: $route.path === '/bookings' ? 'var(--text-color)' : 'transparent', 
-        color: $route.path === '/bookings' ? 'var(--background-color)' : 'var(--text-color)'}"
-          >
-            Bookings
-          </RouterLink>
-          <RouterLink
-            to="/appearance"
-            class="navbar-item"
-            active-class="is-active"
-            @click.prevent="toggleMobileNav"
-            :style="{ backgroundColor: $route.path === '/appearance' ? 'var(--text-color)' : 'transparent', 
-        color: $route.path === '/appearance' ? 'var(--background-color)' : 'var(--text-color)'}"
-          >
-            Appearance
-          </RouterLink>
-          <button
-            :style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)'}"
-            class="button is-small mt-3 ml-3"
-            @click="logout()"
-            
-          >
+<div class="navbar-end" >
+  <RouterLink
+  to="/"
+  class="navbar-item"
+  active-class="is-active"
+  @click.prevent="toggleMobileNav" 
+  :style="{ backgroundColor: $route.path === '/services' ? 'var(--text-color)' : 'transparent', 
+  color: $route.path === '/services' ? 'var(--background-color)' : 'var(--text-color)'}"
+  
+  >
+  New Booking
+</RouterLink>
 
-    Log out {{ storeAuth.user.email }}
-  </button>
-        </div>
-      </div>
-    </div>
-  </nav>
+<RouterLink
+to="/adminServices"
+class="navbar-item"
+active-class="is-active"
+@click.prevent="toggleMobileNav"
+:style="{ backgroundColor: $route.path === '/adminServices' ? 'var(--text-color)' : 'transparent', 
+color: $route.path === '/adminServices' ? 'var(--background-color)' : 'var(--text-color)'}"
+>
+Admin Services
+</RouterLink>
+<RouterLink
+to="/adminDates"
+class="navbar-item"
+active-class="is-active"
+@click.prevent="toggleMobileNav"
+:style="{ backgroundColor: $route.path === '/adminDates' ? 'var(--text-color)' : 'transparent', 
+color: $route.path === '/adminDates' ? 'var(--background-color)' : 'var(--text-color)'}"
+>
+Admin Dates
+</RouterLink>
+<RouterLink
+to="/clients"
+class="navbar-item"
+active-class="is-active"
+@click.prevent="toggleMobileNav"
+style="color: var(--text-color)"
+:style="{ backgroundColor: $route.path === '/clients' ? 'var(--text-color)' : 'transparent', 
+color: $route.path === '/clients' ? 'var(--background-color)' : 'var(--text-color)'}"
+>
+Clients
+</RouterLink>
+<RouterLink
+to="/bookings"
+class="navbar-item"
+active-class="is-active"
+@click.prevent="toggleMobileNav"
+:style="{ backgroundColor: $route.path === '/bookings' ? 'var(--text-color)' : 'transparent', 
+color: $route.path === '/bookings' ? 'var(--background-color)' : 'var(--text-color)'}"
+>
+Bookings
+</RouterLink>
+<RouterLink
+to="/appearance"
+class="navbar-item"
+active-class="is-active"
+@click.prevent="toggleMobileNav"
+:style="{ backgroundColor: $route.path === '/appearance' ? 'var(--text-color)' : 'transparent', 
+color: $route.path === '/appearance' ? 'var(--background-color)' : 'var(--text-color)'}"
+>
+Appearance
+</RouterLink>
+<button
+:style="{ color: 'var(--text-color)', backgroundColor: 'var(--background-color)'}"
+class="button is-small mt-3 ml-3"
+@click="logout()"
+
+>
+
+Log out {{ storeAuth.user.email }}
+</button>
+</div>
+</div>
+</div>
+</nav>
 </template>
 
 <script setup>
 /*
-  imports
+imports
 */
 
-  import { ref, computed, watch } from 'vue'
-  import { useStoreColors } from '@/stores/storeColors.js';
-  import { routerKey, useRoute, useRouter } from 'vue-router';
-  import { useStoreAuth } from '@/stores/storeAuth';
+import { ref, computed, watch } from 'vue'
+import { useStoreColors } from '@/stores/storeColors.js';
+import { routerKey, useRoute, useRouter } from 'vue-router';
+import { useStoreAuth } from '@/stores/storeAuth';
 
 
-    /*
-    store
-    */
-    const storeAuth = useStoreAuth()
+/*
+store
+*/
+const storeAuth = useStoreAuth()
 
 
 
@@ -141,14 +141,14 @@ appearance
 const storeColors = useStoreColors();
 
 /*
-  mobile nav
+mobile nav
 */
 
-  const showMobileNav = ref(false)
-  const toggleMobileNav = () => {
-if (window.innerWidth <= 1023) {
-  showMobileNav.value = !showMobileNav.value
-}
+const showMobileNav = ref(false)
+const toggleMobileNav = () => {
+  if (window.innerWidth <= 1023) {
+    showMobileNav.value = !showMobileNav.value
+  }
 }
 const router = useRouter()
 const route = useRoute();
